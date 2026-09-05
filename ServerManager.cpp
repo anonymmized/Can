fe5ee69caf6dcd7bb095df2e8deb16e7d7c33076
anonymmized <lih0deev@inbox.ru>
2026-09-05 22:11:22 +0300

    Server.[c/h]pp renamed to ServerManager.[c/h]hpp

--- ServerManager.cpp
+++ ServerManager.cpp
@@ -1,4 +1,4 @@
-#include "Server.hpp"
+#include "ServerManager.hpp"
 
 #include <string>
 #include <vector>
--- can.cpp
+++ can.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include "Server.hpp"
+#include "ServerManager.hpp"
 
 int main(int argc, char** argv) {
     try {
